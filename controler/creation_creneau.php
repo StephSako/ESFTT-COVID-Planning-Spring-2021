42e27768be7abc46f1232ce060e5f0209ab891cd
Steph Sako <stephen.sakovitch@orange.fr>
2021-04-11 01:44:56 +0100

    fix show creneau with end date of actual date

--- controler/creation_creneau.php
+++ controler/creation_creneau.php
@@ -6,9 +6,9 @@
     if(!empty($_POST['jour_debut']) && !empty($_POST['jour_fin'])) {
 
         $jour_debut = explode('/', $_POST['jour_debut']);
-        $jour_debut = $jour_debut[2] . '-' . $jour_debut[1] . '-' . $jour_debut[0];
+        $jour_debut = $jour_debut[2] . '-' . $jour_debut[1] . '-' . $jour_debut[0] . ' 23:59:59';
         $jour_fin = explode('/', $_POST['jour_fin']);
-        $jour_fin = $jour_fin[2] . '-' . $jour_fin[1] . '-' . $jour_fin[0];
+        $jour_fin = $jour_fin[2] . '-' . $jour_fin[1] . '-' . $jour_fin[0] . ' 23:59:59';
 
         $horaire_debut = array_fill(0, $nbCreneaux, null);
         $horaire_fin = array_fill(0, $nbCreneaux, null);
@@ -21,7 +21,7 @@
             if (!empty(trim(htmlspecialchars($_POST['jour' . $i])))) {
                 $jour_splited = explode('/', htmlspecialchars($_POST['jour' . $i]));
                 $jour_splited = $jour_splited[2] . '-' . $jour_splited[1] . '-' . $jour_splited[0];
-                $jour[$i] = $jour_splited;
+                $jour[$i] = $jour_splited . ' 23:59:59';
             } else $jour[$i] = null;
         }
 
